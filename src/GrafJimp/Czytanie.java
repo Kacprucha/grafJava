d931888b9a6ef9e8cdaec0870bb26087626d33ab
Karas Kacper <karas.kacper@gmail.com>
2022-04-29 08:13:35 +0200

    Napisanie klasy Czytanie dostosowanej do nowej budowy klasy Graf

--- src/GrafJimp/Czytanie.java
+++ src/GrafJimp/Czytanie.java
@@ -1,30 +1,17 @@
 package GrafJimp;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
 import java.util.InputMismatchException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Czytanie {
     static private String nazwaPliku;
 
-    private char c;
-    private char poprzedniZnak;
-    private int aktualnyWezel, aktualnySasiad, licznikSasiadow;
-    private double waga;
-    final Scanner skanerPliku;
-    final Scanner skanerLinii;
-    private String linia;
+    Czytanie () {
 
-    Czytanie (Graf graf) {
-        this.skanerPliku = new Scanner(nazwaPliku);
-        this.linia = skanerPliku.nextLine();
-        this.skanerLinii = new Scanner(linia);
-        try {
-            graf.setKolumny(skanerLinii.nextInt());
-            graf.setWiersze(skanerLinii.nextInt());
-        }
-        catch(InputMismatchException e) {
-            System.out.println(":((((");
-        }
     }
 
     static void setNazwaPliku(String s) {
@@ -33,5 +20,73 @@ public class Czytanie {
 
     static String getNazwaPliku() {
         return nazwaPliku;
+    }
+
+    public static void czytaj(Graf graf){
+        File wejscie = new File(String.valueOf(Paths.get(nazwaPliku).toAbsolutePath()));
+        try {
+            Scanner skanerPliku = new Scanner(wejscie);
+            String linia = skanerPliku.nextLine();
+            Scanner skanerLinii = new Scanner(linia);
+
+            try {
+                graf.setKolumny(skanerLinii.nextInt());
+                graf.setWiersze(skanerLinii.nextInt());
+
+                graf.setWierzcholki(new Wierzcholek[graf.getKolumny()* graf.getWiersze()]);
+                graf.setKrawedzi(new Krawedz[4*graf.getKolumny()* graf.getWiersze()]);
+
+                int pozycja =0;
+
+                for(int i = 0; i < graf.getKolumny() * graf.getWiersze(); i++) {
+                    linia=skanerPliku.nextLine();
+                    String nowaLinia = linia.replaceAll(":", "");
+
+                    System.out.println(nowaLinia);
+
+                    skanerLinii = new Scanner(nowaLinia).useLocale(Locale.US);
+
+                    Wierzcholek wierzcholek = new Wierzcholek(i);
+
+                    System.out.println(skanerLinii.hasNextDouble());
+                    System.out.println(skanerLinii.hasNextInt());
+
+                    while(skanerLinii.hasNextDouble() && skanerLinii.hasNextInt()){
+                        System.out.println("ta pętla się wykonuje");
+
+                        int sasiad =skanerLinii.nextInt();
+
+                        System.out.println(sasiad);
+
+                        wierzcholek.dodajSąsiada(sasiad, pozycja++);
+
+                        System.out.println("dodano wierzchołek");
+
+                        if(!skanerLinii.hasNextDouble()){
+                            System.out.println(skanerLinii.next());
+                        }
+
+                        double waga = skanerLinii.nextDouble();
+
+                        System.out.println(waga);
+
+                        Krawedz krawedz = new Krawedz(waga);
+                        krawedz.dodajPolaczenie(i, sasiad);
+
+                        System.out.println("dodano połączenie");
+
+                        graf.dodajKrawedz(krawedz);
+                    }
+                    pozycja = 0;
+                    graf.dodajWierzcholek(wierzcholek);
+                }
+            }
+            catch(InputMismatchException e) {
+                System.out.println(":((((");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Nie udało się odnaleźć pliku :<");
+        }
+        graf.wypiszGraf();
     }
 }
