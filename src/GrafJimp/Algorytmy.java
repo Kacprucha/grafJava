b900a53c7d2066b066cbc84cc9b4bb2c93c87284
Karas Kacper <karas.kacper@gmail.com>
2022-05-03 14:20:39 +0200

    Dodanie metody wykonajAlgorytmDjikstry

--- src/GrafJimp/Algorytmy.java
+++ src/GrafJimp/Algorytmy.java
@@ -39,4 +39,62 @@ public class Algorytmy {
             kolor[u.getNumer()] = 2;
         }
     }
+
+    public void wykonajAlgorytmDijkstry(Graf graf, int poczatekDijkstra, double [] odleglosci){
+        int[] poprzednik = new int[graf.getLiczbaWierzchołków()];
+        int[] kolejka = new int [4*graf.getLiczbaWierzchołków()];
+
+        int u;
+
+        int poczatek = 0;
+        int koniec = 0;
+
+        for(Wierzcholek w : graf.getWierzcholki()){
+            odleglosci[w.getNumer()]=Double.MAX_VALUE;
+            poprzednik[w.getNumer()]=-1;
+        }
+
+        odleglosci[poczatekDijkstra] = 0;
+
+        kolejka[koniec++] = poczatekDijkstra;
+        odleglosci[poczatekDijkstra] = 0.0;
+
+        double min = odleglosci[poczatekDijkstra];
+        int min_i = poczatekDijkstra;
+        int tmp;
+        int nrSasiada;
+        Wierzcholek [] tmpWierzcholki = graf.getWierzcholki();
+        while(poczatek != koniec){
+
+            for(int i = poczatek; i < koniec; i++){
+                if(odleglosci[kolejka[i]] <= min){
+                    min = odleglosci[kolejka[i]];
+                    min_i = i;
+                }
+            }
+            tmp = kolejka[poczatek];
+            kolejka[poczatek] = kolejka[min_i];
+            kolejka[min_i] = tmp;
+
+            u = kolejka[poczatek];
+            nrSasiada = 0;
+            for(int i : tmpWierzcholki[u].getNumerySąsiadów()){
+
+                if(i != -1){
+                    for(int j = 0; j < graf.getLiczbaKrawedzi(); j++){
+                        Krawedz k = graf.getKrawedzi()[j];
+                        if(k.getPolaczenie()[0] == u && k.getPolaczenie()[1] == i && odleglosci[i] > odleglosci[u] + k.getWaga()){
+                            odleglosci[i] = odleglosci[u] + k.getWaga();
+                            kolejka[koniec++] = i;
+                            poprzednik[i] = u;
+                            tmpWierzcholki[u].getNumerySąsiadów()[nrSasiada++] = -1;
+                            break;
+                        }
+                    }
+                }
+            }
+            poczatek++;
+        }
+
+    }
 }
